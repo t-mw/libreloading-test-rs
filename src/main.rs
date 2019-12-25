3cc69bf95262b26a034e91d0b06dcf3392e2a1d3
Tobias Mansfield-Williams <contact@tmwhere.com>
2019-12-25 01:11:01 +0000

    Use absolute path

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ use std::path::Path;
 mod interface;
 
 fn main() {
+    let path = std::env::current_dir()
+        .unwrap()
+        .join("target/debug/libreloadable.dylib"); // unknown backtrace
+
     // let path = "libreloadable.dylib"; // complete backtrace
-    let path = "target/debug/libreloadable.dylib"; // unknown backtrace
 
     let lib = Library::new(path).unwrap();
     let api = unsafe { lib.get::<*mut interface::ReloadApi>(b"RELOAD_API").unwrap() };
